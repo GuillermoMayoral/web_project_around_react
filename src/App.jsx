c200e72df3bcded8490f3bdea6e14b2f31ab8025
Mayoral Guillermo <g.mayoral.m@outlook.com>
2026-01-27 21:57:17 -0600

    -HTML transfered
    -CSS transferred
    
    -Folders:Blocks, images, vendor and fonts transferred
    
    --JS code adapted to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './images/title.svg';
+import imgButtonEdit from './images/button-edit.png';
+import imgButtonAdd from './images/profile-add.svg';
+import avatar from './images/placeholder-usuario.png';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="page">
+        <div className="page__size">
+          <header className="header">
+            <div className="header__content">
+              <img className="header__logo" src={logo} alt="Titulo de la pagina"></img>
+              <hr className="header__line"></hr>
+            </div>
+          </header>
+
+          <main className="content">
+            <section className="profile">
+
+              <div className="profile__data">
+                <div className="profile__photo-container">
+                  <img src={avatar} alt="foto de perfil" className="profile__photo"></img>
+                </div>
+                <div className="profile__text">
+                  <div className="profile__header">
+                    <h2 className="profile__name">Memo Mayoral</h2>
+                    <button className="profile__edit-button" type="button">
+                      <img className="profile__edit-image" src={imgButtonEdit} alt="editar nombre y descripcion"></img>
+                    </button>
+                  </div>
+                  <p className="profile__description">Estudiante</p>
+                </div>
+              </div>
+              <button className="profile__button" type="button">
+                <img className="profile__button-image" src={imgButtonAdd} alt="signo de mas"></img>
+              </button>
+            </section>
+            <section className="post">
+              <ul className="post__cards">
+              </ul>
+            </section>
+          </main>
+
+          <footer className="footer">
+            <p className="footer__text">&copy; 2024 Around The U.S.</p>
+          </footer>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
